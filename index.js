8f6a31f239bea5cb24a564b204c491fadf83c310
ursmoore <urs.moore@gmx.de>
2022-05-04 14:43:50 +0200

    html and js file created + starting on charcters

---
+++ index.js
@@ -0,0 +1,18 @@
+const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d"); //c for context
+
+canvas.width = 1024;
+canvas.height = 576;
+
+c.fillRect(0, 0, canvas.width, canvas.height);
+
+class Sprite {
+  constructor(position) {
+    this.position = position;
+  }
+}
+
+const player = new Sprite({
+  x: 0,
+  y: 0,
+});
