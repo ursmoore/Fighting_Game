c6d7a2c9f2b9cb1eaa016c7444485e04e253c2bf
ursmoore <urs.moore@gmx.de>
2022-05-04 15:09:06 +0200

    player and enemy moving

--- index.js
+++ index.js
@@ -7,12 +7,56 @@ canvas.height = 576;
 c.fillRect(0, 0, canvas.width, canvas.height);
 
 class Sprite {
-  constructor(position) {
+  constructor({ position, velocity }) {
     this.position = position;
+    this.velocity = velocity;
+    this.height = 150;
+  }
+
+  draw() {
+    c.fillStyle = "red";
+    c.fillRect(this.position.x, this.position.y, 50, this.height);
+  }
+  update() {
+    this.draw();
+    this.position.y += this.velocity.y;
+
+    if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+      this.velocity.y = 0;
+    }
   }
 }
 
 const player = new Sprite({
-  x: 0,
-  y: 0,
+  position: {
+    x: 0,
+    y: 0,
+  },
+  velocity: {
+    x: 0,
+    y: 10,
+  },
 });
+
+const enemy = new Sprite({
+  position: {
+    x: 800,
+    y: 100,
+  },
+  velocity: {
+    x: 0,
+    y: 0,
+  },
+});
+
+console.log(player);
+
+function animate() {
+  window.requestAnimationFrame(animate); //infinite loop because it calls itself
+  c.fillStyle = "black";
+  c.fillRect(0, 0, canvas.width, canvas.height);
+  player.update();
+  enemy.update();
+}
+
+animate();
